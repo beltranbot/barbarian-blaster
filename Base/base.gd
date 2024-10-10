25c1f69f8c2d934f668e2ce6b22444be0e6f580b
beltranbot <beltranbot@gmail.com>
2024-10-09 20:04:21 -0500

    3.9 introducing properties

--- Base/base.gd
+++ Base/base.gd
@@ -1,13 +1,20 @@
 extends Node3D
 
 @export var max_health: int = 5
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		label_3d.text = str(current_health)
+		print("health was changed")
+		if current_health < 1:
+			get_tree().reload_current_scene()
+			pass
+		pass
 
 @onready var label_3d: Label3D = $Label3D
 
 func _ready() -> void:
-	label_3d.text = str(max_health)
+	current_health = max_health
 
 func take_damage() -> void:
-	max_health -= 1
-	label_3d.text = str(max_health)
-	print("damage dealt to base!")
+	current_health -= 1
