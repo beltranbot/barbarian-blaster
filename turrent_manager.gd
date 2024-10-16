ef1244512645ede633113f10cf620c76254de32b
beltranbot <beltranbot@gmail.com>
2024-10-15 21:19:10 -0500

    3.18 aiming the turrets

--- turrent_manager.gd
+++ turrent_manager.gd
@@ -1,15 +1,11 @@
 extends Node3D
 
 @export var turret: PackedScene
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var new_turret = turret.instantiate()
-	add_child(new_turret)
+@export var enemy_path: Path3D
 
 
 func build_turret(turret_position: Vector3) -> void:
 	var new_turret = turret.instantiate()
 	add_child(new_turret)
 	new_turret.global_position = turret_position
+	new_turret.enemy_path = enemy_path
