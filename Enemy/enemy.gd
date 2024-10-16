50ba1cefbf922958f17c7db5db2fe3191215d2d8
beltranbot <beltranbot@gmail.com>
2024-10-15 21:48:01 -0500

    3.19 damaging enemies

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,8 +1,20 @@
 extends PathFollow3D
 
-@export var speed: float = 20.0
+@export var speed: float = 2.5
+@export var max_health := 50
+
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		printt(current_health, health_in)
+		if current_health < 1:
+			queue_free()
 
 @onready var base = get_tree().get_first_node_in_group("base")
+
+func _ready() -> void:
+	current_health = max_health
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -10,3 +22,5 @@ func _process(delta: float) -> void:
 	if progress_ratio == 1.0:
 		base.take_damage()
 		set_process(false)
+	
+	
